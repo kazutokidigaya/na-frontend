96a8d7216d5a2161f8e320afa61e4c6211c5a36a
karanT <2001karanpt@gmail.com>
2025-01-03 19:08:41 +0530

    pushing the changes for use client on restaurant componenet

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { useRouter, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import BookingModal from "@/components/BookingModal";
 import Loading from "@/components/Loading";
 import { toast } from "react-toastify";
